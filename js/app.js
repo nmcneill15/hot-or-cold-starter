58ba9ac302ce0b6b7c2795bab11c42bb35d4dff0
Nathan McNeill <nmcneill@gmail.com>
2016-06-13 17:59:52 -0500

    added new game function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	
+		var ranNum;
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +12,17 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+		function newGame() {
+			$("#guessList").empty();
+			$("#count").text(0);
+			$("#feedback").text("Make your Guess!");
+			ranNum = Math.floor(Math.random() * 100) + 1;
+		}
+
+		$("a.new").on("click", function(e) {
+			e.preventDefault();
+			newGame();
+		});
+
+
 });
-
-
